7fb2cbd81ab21fa53b24f48cbc518e9650c5fdfd
Riya Rakshit <46257735+RakshitRiya@users.noreply.github.com>
2020-03-18 15:29:58 +0530

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Riya Rakshit`,
+    author: `Riya Rakshit`,
+    description: `I help tech startups from idea phase to high volumes. I design and build
+              scalable solutions, implement internal processes that fit your
+              organization, build teams that produce results.`,
+    siteUrl: `https://github.com/RakshitRiya`,
+    
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Riya Rakshit`,
+        short_name: `CV`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#38B2AC`,
+        display: `standalone`,
+        icon: 'src/assets/site.png'
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        tailwind: true, // Enable tailwindcss support
+      }
+    }
+  ],
+};
